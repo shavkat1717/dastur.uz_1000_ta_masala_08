56387d483e1f183e5776b730cf879cc39f659b92
shavkat_amirov <84470034+shavkat1717@users.noreply.github.com>
2021-11-25 22:05:09 +0500

    Minimum va maksimumga oid masalalar
    Minimum va maksimumga oid masalalar

---
+++ 10_masala.py
@@ -0,0 +1,18 @@
+n=int(input("To'plamning elementlar sonini kiriting: n => "))
+a=int(input("To'plamga tegishli 1-sonni kiriting: a => "))
+max = a
+min = a
+l=1
+j=1
+for i in range(2,n+1):
+    b=int(input(f"To'plamga tegishli {i}-sonni kiriting: p => "))
+    if max<b:
+        max=b
+        j=i
+    if min>b:
+        min=b
+        l=i
+if l<j:  
+    print(f"\nEkstremal(min) element tartibi: {l}.")
+else:
+    print(f"\nEkstremal(max) element tartibi: {j}.")
