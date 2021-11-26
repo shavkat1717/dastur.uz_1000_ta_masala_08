4e1339211d6338dae6574142f5e6579e3858625d
shavkat_amirov <84470034+shavkat1717@users.noreply.github.com>
2021-11-26 18:24:32 +0500

    Minimum va maksimumga oid masalalar
    Minimum va maksimumga oid masalalar

---
+++ 25_masala.py
@@ -0,0 +1,16 @@
+n=int(input("N sonini kiriting: N => "))
+a=int(input("1-sonini kiriting: n => "))
+b=int(input("2-sonini kiriting: n => "))
+y=0
+j=2
+x=a*b
+for i in range(3,n+1):
+    d=int(input(f"{i}-sonni kiriting: n => "))
+    y=b*d    
+    if x>=y:
+        x=y
+        b=d
+        j=i
+    else:
+        b=d
+print(f"Qo'shni tartib raqami {j-1}- va {j}-sonlar ko'paytmasi minimum! Uning qiymati {x} ga teng")
